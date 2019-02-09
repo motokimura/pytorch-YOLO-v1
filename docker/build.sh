13bd31085f920cf411c1e94ba9f652cf8950d40e
Motoki Kimura <motoki.kimura.1990@gmail.com>
2019-02-09 17:35:22 +0900

    Add Dockerfile and utility shell scripts

---
+++ docker/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Set image name
+IMAGE="yolo-ref:latest"
+
+# Get project root dicrectory
+THIS_DIR=$(cd $(dirname $0); pwd)
+PROJ_DIR=`dirname ${THIS_DIR}`
+
+# Buld docker image from project root directory
+cd ${PROJ_DIR} && \
+nvidia-docker build -t ${IMAGE} --build-arg UID=`id -u` -f ${THIS_DIR}/Dockerfile .
+
---
+++ docker/exec.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Exec
+CONTAINER="yolo-ref"
+
+nvidia-docker exec -it ${CONTAINER} /bin/bash
+
